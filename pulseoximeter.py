478da745504d4d8793afa098f27d7144c874e48e
yuzuafro <Nango.Yuzuko@gmail.com>
2020-02-16 20:59:47 +0900

    edit pulseoximeter.py

--- pulseoximeter.py
+++ pulseoximeter.py
@@ -9,16 +9,11 @@ class MAXREFDES117():
     def get(self):
         # MAXREFDES117から値を取得
         m = max30102.MAX30102()
-
-        ir = []
-        red = []
         red, ir = m.read_sequential(1000)
-
         m.shutdown()
 
-        # 取得した値を整形
-        hr = []
-        spo2 = []
+        # 取得した値から心拍数、SpO2を計算
+        hr, spo2 = [], []
         for i in range(37):
             measure = hrcalc.calc_hr_and_spo2(ir[25*i:25*i+100], red[25*i:25*i+100])
             # True のものを取り出す
